6088b1e0c1582392c1f3e9be440063aa2ddab05d
Cortes-Ces <70327348+Cortes-Ces@users.noreply.github.com>
2020-09-06 21:48:00 -0400

    Create csvSpotify.java

---
+++ src/csvSpotify.java
@@ -0,0 +1,46 @@
+import java.io.*;
+
+
+/**
+ * Tried creating a nested Java array.
+ * This was the best I could do.
+ * I need to further refresh my memory on 2d arrays
+ */
+public class csvSpotify
+{
+    public static void main(String[]args) {
+
+        // Read csv Spotify file located on my desktop
+        // Download csv file, save on desktop, and add file location in csvFile = " "
+        String csvFile = "C:\\Users\\corte\\OneDrive\\Desktop\\viral-global-daily-latest.csv";
+
+
+
+        String line = "";
+
+            // Try catch block when reading and opening files
+            try {
+                BufferedReader br = new BufferedReader(new FileReader(csvFile));
+                // Create text file
+                File theFile = new File("Artists-Week-Of-09-01-2020.txt");
+                PrintStream out = new PrintStream(new FileOutputStream(theFile));
+                // While next line is not null parse the line with commas and store it into
+                // values array
+                while ((line = br.readLine()) != null) {
+                    String[] values = line.split(",");
+                    // Print artists name located in [2] to console
+                    System.out.println(values[2]);
+                    // Print artists name to text file report
+                    out.print(values[2] + "\n");
+                }
+                        // Close PrintStream
+                       out.close();
+                } catch(FileNotFoundException e){
+                    e.printStackTrace();
+                }   catch(IOException e){
+                    e.printStackTrace();
+                }
+
+
+    }
+}
