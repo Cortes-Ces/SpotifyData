901e9bbd4b9f7953023dfaa808777262902bba93
Cortes-Ces <70327348+Cortes-Ces@users.noreply.github.com>
2020-10-06 10:39:34 -0400

    Update csvSpotify.java

--- src/csvSpotify.java
+++ src/csvSpotify.java
@@ -7,45 +7,54 @@ import java.io.*;
  * EmplId:23335766
  * Name:Cesar Cortes
  *
- * I tried creating a 2D Java array.
- * This was the best I could do.
- * I need to further refresh my memory on 2d arrays
- */
-public class csvSpotify
-{
-    public static void main(String[]args) {
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
 
-        // Read csv Spotify file located on my desktop
-        // Download csv file, save on desktop, and add file location in csvFile = " "
-        String csvFile = "C:\\Users\\corte\\OneDrive\\Desktop\\viral-global-daily-latest.csv";
+public class SpotifyNew {
+   public static void main(String[] args) {
 
+       // Read csv Spotify file located on my desktop
+       // Download csv file, save on desktop, and add file location in csvFile = " "
+       String csvFile = "C:\\Users\\corte\\OneDrive\\Desktop\\viral-global-daily-latest.csv";
 
+       String line = "";
+       // created one list to add the all the artist name's of csv file
+       List<String> artistList = new ArrayList<String>();
+       // Try catch block when reading and opening files
+       try {
+           BufferedReader br = new BufferedReader(new FileReader(csvFile));
+           // Create text file
+           File theFile = new File("Artists-Week-Of-09-01-2020.txt");
+           PrintStream out = new PrintStream(new FileOutputStream(theFile));
+           // While next line is not null parse the line with commas and store it into values array
+           while ((line = br.readLine()) != null) {
+               String[] values = line.split(",");
+               // Print artists name located in [2] to console
 
-        String line = "";
+               // adding artist name into the list
+               // trim method to remove the any white spaces in the starting or ending of the
+               // name.
+               artistList.add(values[2].trim());
 
-            // Try catch block when reading and opening files
-            try {
-                BufferedReader br = new BufferedReader(new FileReader(csvFile));
-                // Create text file
-                File theFile = new File("Artists-Week-Of-09-01-2020.txt");
-                PrintStream out = new PrintStream(new FileOutputStream(theFile));
-                // While next line is not null parse the line with commas and store it into
-                // values array
-                while ((line = br.readLine()) != null) {
-                    String[] values = line.split(",");
-                    // Print artists name located in [2] to console
-                    System.out.println(values[2]);
-                    // Print artists name to text file report
-                    out.print(values[2] + "\n");
-                }
-                        // Close PrintStream
-                       out.close();
-                } catch(FileNotFoundException e){
-                    e.printStackTrace();
-                }   catch(IOException e){
-                    e.printStackTrace();
-                }
+           }
+           // Collections.sort will do ascending order of the elements
+           Collections.sort(artistList);
+           // iterating over the list
+           for (String artistName : artistList) {
+               // printing sorted artist names into the file
+               out.print(artistName + "\n");
+               // printing into the console
+               System.out.println(artistName);
+           }
+           // Close PrintStream
+           out.close();
+       } catch (FileNotFoundException e) {
+           e.printStackTrace();
+       } catch (IOException e) {
+           e.printStackTrace();
+       }
 
-
-    }
+   }
 }
